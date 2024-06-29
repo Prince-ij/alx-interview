7992c02584b815c9cbb9ecb6bd4c8c01316294d2
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-29 17:53:26 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module for determining if all locked boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    :param boxes: List of lists, where each sublist contains keys for other boxes.
+    :type boxes: list
+    :return: True if all boxes can be unlocked, otherwise False.
+    :rtype: bool
+    """
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is initially unlocked
+    keys = [0]  # Start with the keys from the first box
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
